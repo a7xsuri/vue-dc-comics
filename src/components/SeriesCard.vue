b0d588ab8a3bf54a8291de1e4d7ab6ac51630909
a7xsuri <leonardofilipposurico@gmail.com>
2022-05-05 22:32:52 +0200

    finish

---
+++ src/components/SeriesCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="ThumbCard">
+        <img :src="immagine" :alt="titolo">
+        <h3>{{titolo}}</h3>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'SeriesCard',
+    data() {
+        return{
+
+        }
+    },
+    props: ['immagine', 'titolo']
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/StyleAbsolute';
+
+.ThumbCard{
+    width:15%;
+}
+
+h3{
+    color:white;
+}
+
+img{
+    width: 150px;
+    height: 150px;
+    object-fit: cover;
+}
+
+</style>
